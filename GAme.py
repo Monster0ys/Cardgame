5a90895058e15d388c7e3ca4d178d68cb8ee84cc
Monster0ys <sobolev260403@gmail.com>
2024-12-02 00:13:24 +0300

    Deck of cards is created

--- GAme.py
+++ GAme.py
@@ -1,1 +1,9 @@
-print("CardGAMe")
+colors = ["Red", "Green", "Blue", "Yellow"]
+arr=[]
+for color in colors:    
+    for num in range(10):
+        arr.append(f"{num} {color}")
+        arr.append(f"{num} {color}")
+print('\n'.join(arr))
+
+   
