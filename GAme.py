385b01c58d3ec0cf681e4e1d69583b67878f5dca
Monster0ys <sobolev260403@gmail.com>
2024-12-15 20:38:18 +0300

    adding a function in which the player takes a card from the table

--- GAme.py
+++ GAme.py
@@ -7,19 +7,19 @@ class Card():
     def __init__(self,num,color):
         self.number=num
         self.color=color
-arr:list[Card]=[]
+deck:list[Card]=[]
         
 for color in colors:    
     for num in range(10):
-        arr.append(Card(num, color))
-        arr.append(Card(num, color))
+        deck.append(Card(num, color))
+        deck.append(Card(num, color))
 
-def shuffle_array(arr):
-    for i in range(len(arr) -1,0,-1):   # Идем с конца массива
+def shuffle_deck(deck):
+    for i in range(len(deck)-1,0,-1):   # Идем с конца массива
         j = randrange(i+1)              # Выбираем случайный индекс от 0 до i включительно
-        arr[i], arr[j] = arr[j], arr[i] # Меняем местами элементы
+        deck[i], deck[j] = deck[j], deck[i] # Меняем местами элементы
 
-shuffle_array(arr)
+shuffle_deck(deck)
 
 def check_card_compatibility(card_on_table:Card, players_card:Card):
     if card_on_table.number == players_card.number or card_on_table.color == players_card.color:
@@ -28,14 +28,33 @@ def check_card_compatibility(card_on_table:Card, players_card:Card):
 
 num_players = 3
 num_of_card = 6
-cards_on_table=[arr.pop()]
+cards_on_table=[deck.pop()]
 players_hands=[]
 for i in range(num_players):
     hand=[]
     for _ in range(num_of_card):
-        hand.append(arr.pop())
+        hand.append(deck.pop())
     players_hands.append(hand)
 
-print(cards_on_table[0].number,cards_on_table[0].color,players_hands[0][0].number, players_hands[0][0].color)
-print(check_card_compatibility(cards_on_table[0],players_hands[0][0]))
-   
+def players_turn(num_player,num_card):
+    if check_card_compatibility(cards_on_table[-1],players_hands[num_player][num_card]):
+        cards_on_table.append(players_hands[num_player].pop(num_card))
+        return True
+    else:return False
+
+def add_card_to_player(num_player):
+    global deck
+    global cards_on_table
+    if len(deck) > 0:
+        players_hands[num_player].append(deck.pop())
+        return True
+    elif len(cards_on_table)>1:
+        deck=cards_on_table[0:-1]
+        cards_on_table=cards_on_table[-1:]
+        shuffle_deck(deck)
+        players_hands[num_player].append(deck.pop())
+        return True
+    else:return False
+
+for i in range(100):
+    print(add_card_to_player(0))
